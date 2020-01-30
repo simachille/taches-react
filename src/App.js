efa33c79ce616b0a6684c44650dbb5d822f0cfee
achille <simachille@yahoo.fr>
2020-01-30 13:55:35 +0100

    page taches

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import "./App.css";
+import Tasks from "./components/tasks/Tasks";
 
 function App() {
+  // Variable
+  const welcome = "Bienvenue dans la formation REACT";
+
   return (
-    <h1 className="App" style={{ textAlign: "center" }}>
-      Bienvenue dans la formation REACT
-    </h1>
+    <section className="app-wrapper">
+      <Header />
+      <main>
+        <Tasks />
+      </main>
+      <Footer />
+    </section>
   );
 }
 export default App;
