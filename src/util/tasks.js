838ba67e36f36562e99cea23f97248f9862d6654
achille <simachille@yahoo.fr>
2020-01-31 11:27:37 +0100

    Tag binding

---
+++ src/util/tasks.js
@@ -0,0 +1,143 @@
+/*
+[
+  '{{repeat(5, 7)}}',
+  {
+    id: '{{index()}}',
+    title:  
+      '{{lorem(4, "words")}}',
+    users: [
+      '{{repeat(3)}}',
+      {
+        id: '{{index()}}',
+        firstName: '{{firstName()}}',
+        lastName: '{{surname()}}',
+        email: '{{email()}}'
+      }
+    ]
+   
+    
+  }
+]
+*/
+export const tasks_data = [
+  {
+    id: 0,
+    title: "excepteur eu est nisi",
+    users: [
+      {
+        id: 0,
+        firstName: "Medina",
+        lastName: "Blair",
+        email: "medinablair@fleetmix.com"
+      },
+      {
+        id: 1,
+        firstName: "Burns",
+        lastName: "Langley",
+        email: "burnslangley@fleetmix.com"
+      },
+      {
+        id: 2,
+        firstName: "Pierce",
+        lastName: "Schwartz",
+        email: "pierceschwartz@fleetmix.com"
+      }
+    ]
+  },
+  {
+    id: 1,
+    title: "aute tempor magna irure",
+    users: [
+      {
+        id: 0,
+        firstName: "Ellison",
+        lastName: "Wise",
+        email: "ellisonwise@fleetmix.com"
+      },
+      {
+        id: 1,
+        firstName: "Nadia",
+        lastName: "Blackwell",
+        email: "nadiablackwell@fleetmix.com"
+      },
+      {
+        id: 2,
+        firstName: "Madge",
+        lastName: "Beasley",
+        email: "madgebeasley@fleetmix.com"
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "culpa culpa sit commodo",
+    users: [
+      {
+        id: 0,
+        firstName: "Carla",
+        lastName: "Black",
+        email: "carlablack@fleetmix.com"
+      },
+      {
+        id: 1,
+        firstName: "Hill",
+        lastName: "Roberson",
+        email: "hillroberson@fleetmix.com"
+      },
+      {
+        id: 2,
+        firstName: "Rios",
+        lastName: "Shaffer",
+        email: "riosshaffer@fleetmix.com"
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "pariatur duis id excepteur",
+    users: [
+      {
+        id: 0,
+        firstName: "Wooten",
+        lastName: "Goodwin",
+        email: "wootengoodwin@fleetmix.com"
+      },
+      {
+        id: 1,
+        firstName: "Adeline",
+        lastName: "Turner",
+        email: "adelineturner@fleetmix.com"
+      },
+      {
+        id: 2,
+        firstName: "Mercado",
+        lastName: "Sellers",
+        email: "mercadosellers@fleetmix.com"
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: "eu esse ullamco qui",
+    users: [
+      {
+        id: 0,
+        firstName: "Marjorie",
+        lastName: "Snyder",
+        email: "marjoriesnyder@fleetmix.com"
+      },
+      {
+        id: 1,
+        firstName: "Deloris",
+        lastName: "Cole",
+        email: "deloriscole@fleetmix.com"
+      },
+      {
+        id: 2,
+        firstName: "Workman",
+        lastName: "Robertson",
+        email: "workmanrobertson@fleetmix.com"
+      }
+    ]
+  }
+];
