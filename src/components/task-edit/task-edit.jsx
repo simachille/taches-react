c86627b0e6d35a191397f0afc35e7b2d6952189c
achille <simachille@yahoo.fr>
2020-02-03 15:47:34 +0100

    validation des formulaires

--- src/components/task-edit/task-edit.jsx
+++ src/components/task-edit/task-edit.jsx
@@ -9,6 +9,23 @@ const TaskEdit = ({ match }) => {
   const { register, handleSubmit, errors } = useForm();
 
   const read = async () => {
+    let promise = fetch(url, {
+      method: "GET", //ou POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "text/plain;charset=UTF-8" //pour un corps de type chaine
+      },
+      body: undefined, //ou string, FormData, Blob, BufferSource, ou URLSearchParams
+      referrer: "about:client", //ou "" (pas de réferanr) ou une url de l'origine
+      referrerPolicy: "no-referrer-when-downgrade", //ou no-referrer, origin, same-origin...
+      mode: "cors", //ou same-origin, no-cors
+      credentials: "same-origin", //ou omit, include
+      cache: "default", //ou no-store, reload, no-cache, force-cache, ou only-if-cached
+      redirect: "follow", //ou manual ou error
+      integrity: "", //ou un hash comme "sha256-abcdef1234567890"
+      keepalive: false, //ou true pour que la requête survive à la page
+      signal: undefined //ou AbortController pour annuler la requête
+    });
+
     const id = match.params.id;
     const response = await fetch(TASKS_URL + "/" + id);
     const data = await response.json();
